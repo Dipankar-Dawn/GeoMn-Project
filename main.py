6d1c4e9bb6dc3e94c8f074a6feb498ffaa239e54
Dipankar-Dawn <dawndipankar707@gmail.com>
2026-09-12 19:19:47 +0530

    Revert "Update main.py"
    This reverts commit bcafcf2a37486d97229b5e533e43490b375dd9b2.

--- main.py
+++ main.py
@@ -289,7 +289,7 @@ def get_live_weather(state, district):
         geocode_response = requests.get(
             geocode_url,
             params=geocode_params,
-            timeout=4
+            timeout=10
         )
 
         geocode_response.raise_for_status()
@@ -328,7 +328,7 @@ def get_live_weather(state, district):
         weather_response = requests.get(
             weather_url,
             params=weather_params,
-            timeout=4
+            timeout=10
         )
 
         weather_response.raise_for_status()
